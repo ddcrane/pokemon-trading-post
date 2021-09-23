c26b583c7c8cf4b0f55e20ada113d2ef2654551f
Brian Kaufman <bpkaufman4@gmail.com>
2021-09-22 20:22:01 -0500

    created post model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// Create the user model
 class User extends Model {
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
