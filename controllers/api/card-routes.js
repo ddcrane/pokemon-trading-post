1076629b1437c3ef6356757ade8d25a04c85a13b
Will Knowles <will@willknowles.com>
2021-09-22 20:35:55 -0500

    initial card controller

---
+++ controllers/api/card-routes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Card } = require('../../models');
+
+// authentication
+const authentication = require('../../utils/auth');
+
+// single card
+router.get('/:id', (req, res) => {
+    Card.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            // attributes
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbCardData => {
+        if (!dbCardData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbCardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// create
+router.post('/', authentication, (req, res) => {
+    // expects {}
+    Card.create({
+        // Card properties
+    })
+    .then(dbCardData => res.json(dbCardData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// update
+router.put('/:id', authentication, (req, res) => {
+    Card.update(req.body,{
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCardData => {
+        if (!dbCardData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbCardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// destroy
+router.delete('/:id', authentication, (req, res) => {
+    Card.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCardData => {
+        if (!dbCardData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbCardData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
