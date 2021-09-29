9cb13b0fd878f1fe4cf7a6a667508854a287ec96
Brian Kaufman <bpkaufman4@gmail.com>
2021-09-29 12:07:48 -0500

    added klass to post-card-seeds

--- seeds/post-card-seeds.js
+++ seeds/post-card-seeds.js
@@ -3,11 +3,13 @@ const { PostCard } = require('../models');
 const postCardData = [
     {
         post_id: "1",
-        card_id: "1"
+        card_id: "1",
+        klass: "true"
     },
     {
         post_id: "2",
-        card_id: "2"
+        card_id: "2",
+        klass: "false"
     }
 ];
 
